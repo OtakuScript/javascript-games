0b3352ded97e1428f526a6ec30449fed7fd82829
ASL <1076976275@qq.com>
2025-03-16 23:31:26 +0800

    developing gomoku

--- 1.gomoku/index.js
+++ 1.gomoku/index.js
@@ -14,20 +14,56 @@ let tdWidth; // 格子宽度
 let color = 'black'; // 棋子颜色
 let isGameOver = false; // 游戏是否结束
 
-function checkWinner() {
-  for (let i = 0; i < chesses.length; i++) {
-    let chess = chesses[i];
+function checkDirection(chessPoint, direction) {}
+
+function checkWinner(chessPoint) {
+  let winChessArr = [chessPoint];
+
+  // 判断棋子和其各个方向上的棋子是否可以连成5个
+  let chessBoardWidth = doms.chessBoard.clientWidth;
+  let left = chessPoint.x / tdWidth >= 4;
+  let right = (chessBoardWidth - chessPoint.x) / tdWidth >= 4;
+  let up = chessPoint.y / tdWidth >= 4;
+  let down = (chessBoardWidth - chessPoint.y) / tdWidth >= 4;
+
+  // 向各个方向判断：左右上下，左上，右上，左下，右下
+  if (left) {
+    for (let i = 1; i < 5; i++) {
+      let chess = chesses.find(item => {
+        return (
+          item.x === (chessPoint.x - i * tdWidth).toFixed(2) &&
+          item.y === chessPoint.y &&
+          item.color === chessPoint.color
+        );
+      });
+      if (chess) {
+        winChessArr.push(chess);
+      }
+    }
+    if (winChessArr.length === 5) {
+      return winChessArr;
+    }
+  } else if (right) {
+  } else if (up) {
+  } else if (down) {
+  } else if (left && up) {
+  } else if (right && up) {
+  } else if (left && down) {
+  } else if (right && down) {
+  } else {
+    return false;
   }
 }
 
 function paintChess(chessPoint) {
   let chess = document.createElement('div');
-  chess.className = 'chess';
+  chess.className = 'chess ' + chessPoint.color;
+  chess.setAttribute('data-x', chessPoint.x);
+  chess.setAttribute('data-y', chessPoint.y);
   chess.style.left = chessPoint.x + 'px';
   chess.style.top = chessPoint.y + 'px';
   chess.style.width = tdWidth * 0.9 + 'px';
   chess.style.height = tdWidth * 0.9 + 'px';
-  chess.style.backgroundColor = chessPoint.color;
   doms.chessBoard.appendChild(chess);
 }
 
@@ -44,6 +80,11 @@ function checkMove(chessPoint) {
     chessPoint.color = color;
     chesses.push(chessPoint);
     paintChess(chessPoint);
+    let isHasWinner = checkWinner(chessPoint);
+    console.log('isHasWinner', isHasWinner);
+    if (isHasWinner && isHasWinner.length === 5) {
+      alert('winner');
+    }
   }
 }
 
@@ -101,6 +142,9 @@ function initChessBoard() {
         };
         chessPoint = leftBottom;
       }
+      chessPoint.x = chessPoint.x.toFixed(2);
+      chessPoint.y = chessPoint.y.toFixed(2);
+      // console.log(chessPoint);
       checkMove(chessPoint);
     }
   });
